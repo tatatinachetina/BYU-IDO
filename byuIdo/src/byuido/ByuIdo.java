fb579d8c7ec5a050b6ef3a1dff79aa06dd25db67
BigfatBirds <35468285+BigfatBirds@users.noreply.github.com>
2018-05-15 13:17:46 -0600

    5/15/2018 creating classes, BETTER

--- byuIdo/src/byuido/ByuIdo.java
+++ byuIdo/src/byuido/ByuIdo.java
@@ -5,6 +5,8 @@
  */
 package byuido;
 
+import citbyui.cit260.model.Game;
+
 /**
  *
  * @author tanya
@@ -16,6 +18,11 @@ public class ByuIdo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        Game game = new Game();
+        game.setCurrentScore(20.75);
+        game.setTotalDays(10);
+        System.out.println(game.toString());
     }
     
 }
