97cdc5043772f83ea06542baf5fe9c01b148a6aa
BigfatBirds <35468285+BigfatBirds@users.noreply.github.com>
2018-05-31 13:37:05 -0600

    Jake Edit 5/31/2018

---
+++ byuIdo/src/citbyui/cit260/control/challengeSceneControl.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package citbyui.cit260.control;
+
+/**
+ *
+ * @author Jake
+ */
+public class challengeSceneControl {
+
+    public static double calculateInterest(double borrowed, double interestRate, long years) {
+        if (borrowed <= 0) {
+            return -1;
+        }
+        if (interestRate <= 0) {
+            return -2;
+        }
+        if (years <= 0) {
+            return -3;
+        }
+        double interest = borrowed * (interestRate/100) * years;
+        return interest;
+    }
+}
