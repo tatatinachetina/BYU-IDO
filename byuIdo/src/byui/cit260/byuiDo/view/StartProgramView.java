47bb2b587dfb5f81c32f7cceb6273728b90b217d
BigfatBirds <35468285+BigfatBirds@users.noreply.github.com>
2018-06-08 14:01:56 -0600

    Jake Edit June/8th/2018

--- byuIdo/src/byui/cit260/byuiDo/view/StartProgramView.java
+++ byuIdo/src/byui/cit260/byuiDo/view/StartProgramView.java
@@ -5,42 +5,73 @@
  */
 package byui.cit260.byuiDo.view;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Jake
  */
 public class StartProgramView {
-    
+
     public StartProgramView() {
     }
-   public void displayStartProgramView(){
-   
-       boolean endOfView = false;
- do {
-    String[] inputs = this.getInputs();
- if (inputs.length == 0 || inputs.equals('Q') || inputs.equals('q')){
-endOfView = doAction(inputs);}
- }
-while (endOfView != true); 
-         {
-   
-   }
- 
-   
-}
+
+    public void displayStartProgramView() {
+
+        boolean endOfView = false;
+        do {
+            String[] inputs = this.getInputs();
+            if (inputs.length == 0 || inputs.equals('Q') || inputs.equals('q')) {
+                return;
+            }
+            endOfView = doAction(inputs);
+
+        } while (endOfView != true);
+        {
+        }
+
+    }
 
     private String[] getInputs() {
-        System.out.println("");
-        System.out.println("");
+
+        //inputs = new String array one element long 
         String[] inputs = new String[1];
-        inputs[0] = "testInputs";
+
+        //Display a description of the view 
+        System.out.println("**** Get Inputs ***");
+
+        //valid = false 
+        boolean valid = false;
+
+        //WHILE valid == false (no input value has been entered) 
+        while (valid == false) {
+            //Display the prompt message 
+            System.out.println("**** Display the prompt message  ***");
+
+            //Get the value entered from the keyboard 
+            Scanner inFile;
+            inFile = new Scanner(System.in);
+            
+            //Trim off leading and trailing blanks from the value 
+            inputs[0] = inFile.nextLine().trim();
+
+            
+            //IF length of the value < 1 then
+            if (inputs[0].length() < 1) //Display "You must enter a non-blank value”
+            {
+                System.out.println("You must enter a non-blank value.");
+                continue;
+            }
+            valid = true;
+        }
+
         return inputs;
     }
 
     private boolean doAction(String[] inputs) {
-        System.out.println("doAction() called");
+        System.out.println("**** doAction() called ***");
         System.out.println("\tinputs = " + inputs[0]);
-        
+
         return true;
     }
 }
