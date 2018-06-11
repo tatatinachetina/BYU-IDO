b2539e4d364236bce3862a9cb34ef05d5f29c5a9
BigfatBirds <35468285+BigfatBirds@users.noreply.github.com>
2018-06-11 14:01:12 -0600

    Jake Edit June/11th/2018

--- byuIdo/src/byui/cit260/byuiDo/view/MainMenuView.java
+++ byuIdo/src/byui/cit260/byuiDo/view/MainMenuView.java
@@ -5,6 +5,9 @@
  */
 package byui.cit260.byuiDo.view;
 
+import byuido.ByuIdo;
+import java.util.Scanner;
+
 /**
  *
  * @author Jake
@@ -22,18 +25,67 @@ class MainMenuView {
             endOfView = doAction(inputs);
 
         } while (endOfView != true);
-        {
-        }
 
     }
 
     private String[] getInputs() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+        String[] inputs = new String[1];
+
+        System.out.println("*** Display instructions ***");
+        boolean valid = false;
+        while (valid == false) {
+            System.out.println("Display the prompt message");
+            Scanner inFile;
+            inFile = new Scanner(System.in);
+
+            //Trim off leading and trailing blanks from the value 
+            inputs[0] = inFile.nextLine().trim();
+            if (inputs[0].length() < 1) {
+                System.out.println("You must enter a value");
+                continue;
+            }
+            valid = true;
+        }
+
+        return inputs;
     }
 
+    //doAction(inputs): boolean 
     private boolean doAction(String[] inputs) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        String menuItem = inputs[0];
+        menuItem = menuItem.toUpperCase();
+        switch (menuItem) {
+            case "N":
+                this.startNewGame();
+                break;
+            case "R":
+                this.restartGame();
+                break;
+            case "H":
+                this.getHelp();
+                break;
+            case "Q":
+                return true;
+              
+            default: System.out.println("Invalid menu item");
+        }
+        return false;
     }
-    
+
+    private void startNewGame() {
+      GameControle game = new Game();
+        GameControl.createNewGame(ByuIdo.getPlayer());
+       gameMenuView = createNewGame
+       gameMenuView.displayGameMenuView();
+    }
+
+    private void restartGame() {
+       
+    }
+
+    private void getHelp() {
+   
+    }
+
 }
-  
