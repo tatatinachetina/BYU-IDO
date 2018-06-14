9107b89ef7245ccc734048df965583f60568b28b
BigfatBirds <35468285+BigfatBirds@users.noreply.github.com>
2018-06-14 13:48:35 -0600

    Jake Edit June/14th/2018

--- byuIdo/src/byui/cit260/byuiDo/view/EndShiftView.java
+++ byuIdo/src/byui/cit260/byuiDo/view/EndShiftView.java
@@ -30,38 +30,66 @@ class EndShiftView {
     }
 
     private String[] getInputs() {
-        String[] inputs = new String[1];
+        String[] inputs = new String[2];
         System.out.println("****************************************************");
-        System.out.println("* P - Perform good deeds                           *");
+        System.out.println("* Enter hours worked and how much we pay you                          *");
         System.out.println("****************************************************");
         boolean valid = false;
         while (valid == false) {
             System.out.println("Enter hours");
             Scanner hours;
             hours = new Scanner(System.in);
-            System.out.println("Enter pay");
-            Scanner pay;
-            pay = new Scanner(System.in);
 
             //Trim off leading and trailing blanks from the value 
             inputs[0] = hours.nextLine().trim();
             if (inputs[0].length() < 1) {
                 System.out.println("Enter letter below:");
                 continue;
-                
+
+            }
+            valid = true;
+        }
+        valid = false;
+        while (valid == false) {
+            System.out.println("Enter pay");
+            Scanner pay;
+            pay = new Scanner(System.in);
+
+            //Trim off leading and trailing blanks from the value 
+            inputs[1] = pay.nextLine().trim();
+            if (inputs[0].length() < 1) {
+                System.out.println("Enter letter below:");
+                continue;
+
             }
             valid = true;
         }
 
         return inputs;
+
     }
 
     private boolean doAction(String[] inputs) {
-double hours = Double.parseDouble(inputs[0]);
-double pay = Double.parseDouble(inputs[0]);
-double wage = jobControl.calculateWage(hours, pay);
+        double hours = Double.parseDouble(inputs[0]);
+        double pay = Double.parseDouble(inputs[1]);
+        double wage = jobControl.calculateWage(hours, pay);
 
+        if (wage == -1) {
+            System.out.println("Your hours are wrong!!!");
+            return false;
+        }
+        if (wage == -2) {
+            System.out.println("wait, we need to pay you!!!");
+            return false;
+        }
+
+        if (wage < 5) {
+            System.out.println("You need more money!!!");
+            return false;
+        }
+
+        System.out.println("your wage is: " + wage);
 //double hours = jobControl.calculateWage(0, 0)
-return true;
+        return true;
     }
 }
