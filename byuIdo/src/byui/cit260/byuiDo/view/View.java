1e6ea4a8a289d4ab4d2aec47936c5eef23ceef86
BigfatBirds <35468285+BigfatBirds@users.noreply.github.com>
2018-06-20 12:49:36 -0600

    Jake, first one! June/20th/2018

--- byuIdo/src/byui/cit260/byuiDo/view/View.java
+++ byuIdo/src/byui/cit260/byuiDo/view/View.java
@@ -13,5 +13,22 @@ public abstract class View implements ViewInterface {
 
     public View() {
     }
-    
+    @Override
+    public void display() {
+        
+        boolean endOfView = false;
+        do {
+            String[] inputs = this.getInputs();
+            if (inputs[0].toUpperCase().equals("Q") || inputs.length == 0) {
+                return;
+            }
+            endOfView = doAction(inputs);
+
+        } while (endOfView != true);
+        
+    }
+    @Override
+        public String getInputs(String promptMessage){
+        
+    }
 }
